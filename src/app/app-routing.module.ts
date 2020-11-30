cc16de14a955058faafee2676d1383117fe1d59c
Eduardo ASTOLFI <eduardo.astolfi@prima-solutions.com>
2020-11-30 09:21:49 +0100

    feat: create a main home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
     {
         path: '',
-        redirectTo: 'login',
+        redirectTo: 'home',
         pathMatch: 'full'
     }, {
         path: 'login',
         loadChildren: async () => (await import('./pages/login/login.module')).LoginModule
+    }, {
+        path: 'home',
+        loadChildren: async () => (await import('./pages/home/home.module')).HomeModule
     }, {
         path: 'wish-counter',
         loadChildren: async () => (await import('./pages/wish-counter/wish-counter.module')).WishCounterModule
