06a6577676f2b07a9143db6286bc3416c19efbe7
Eduardo ASTOLFI <eduardo.astolfi@prima-solutions.com>
2020-11-30 09:21:49 +0100

    feat: create a main home page

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { BottomNavService } from 'src/app/components/bottom-nav/bottom-nav.service';
+import { AuthService, User } from 'src/app/services/auth.service';
+
+@Component({
+    selector: 'wc-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+    constructor(
+        private readonly authService: AuthService,
+        private readonly bottomNavService: BottomNavService,
+    ) { }
+
+    ngOnInit(): void {
+        this.bottomNavService.setItems([
+            {icon: 'home', label: 'Home', routerLink: 'home'},
+            {icon: 'wc-wish', svgIcon: true, label: 'Wish Counter', routerLink: 'wish-counter'},
+        ]);
+    }
+
+    public get currentUser(): User {
+        return this.authService.currentUser.value;
+    }
+
+    public logout(): void {
+        this.authService.signOut()
+        .then(window.location.reload)
+        .catch(console.warn);
+    }
+
+}
