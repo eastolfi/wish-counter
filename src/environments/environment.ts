18d2ee73b8023f49f5261bd7c38c74c68a1422a8
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2020-12-19 22:26:04 +0100

    fix(auth): add the missing firebase config for oauth

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,16 @@
 
+// --- DO NOT MODIFY ---
+// THE CONTENT OF THE FILE IS GENERATED AUTOMATICALLY FILE.
+// VIEW scripts/setenv.js
 export const environment = {
    production: false,
    firebase: {
-        apiKey: 'AIzaSyBs491aw277wMXFqN8fjGcw54mKohmeH14',
+        apiKey: '',
         projectId: 'wish-counter-985b1',
+        authDomain: 'wish-counter-985b1.firebaseapp.com',
+        databaseURL: 'https://wish-counter-985b1.firebaseio.com',
+        storageBucket: 'wish-counter-985b1.appspot.com',
+        messagingSenderId: '1064305023356',
+        appId: '1:1064305023356:web:3762c467ce9018afb12cf9'
     }
 };
