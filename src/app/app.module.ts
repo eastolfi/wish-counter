c3ba757b4fb60b4b98aacad2aa7158d0e830a60c
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2020-12-19 18:56:29 +0100

    feat(auth): replace the current login screen with a prebuilt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { MatIconRegistry } from '@angular/material/icon';
+import { NgxAuthFirebaseUIConfig, NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
 
 import { environment } from '../environments/environment';
 
@@ -22,6 +23,23 @@ import { AppComponent } from './app.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
     return new TranslateHttpLoader(http, './assets/locale/', '.json');
+}
+
+const firebaseUiConfig: NgxAuthFirebaseUIConfig = {
+    enableFirestoreSync: false, // enable/disable autosync users with firestore
+    toastMessageOnAuthSuccess: false, // whether to open/show a snackbar message on auth success - default : true
+    toastMessageOnAuthError: true, // whether to open/show a snackbar message on auth error - default : true
+    authGuardFallbackURL: '/login', // url for unauthenticated users - to use in combination with canActivate feature on a route
+    // authGuardLoggedInURL: '/loggedin', // url for authenticated users - to use in combination with canActivate feature on a route
+    passwordMaxLength: 60, // `min/max` input parameters in components should be within this range.
+    passwordMinLength: 6, // Password length min/max in forms independently of each componenet min/max.
+    // Same as password but for the name
+    nameMaxLength: 50,
+    nameMinLength: 2,
+    // If set, sign-in/up form is not available until email has been verified.
+    // Plus protected routes are still protected even though user is connected.
+    guardProtectedRoutesUntilEmailIsVerified: false,
+    enableEmailVerification: false, // default: true
 }
 
 @NgModule({
@@ -44,6 +62,7 @@ export function HttpLoaderFactory(http: HttpClient) {
                 deps: [HttpClient]
             }
         }),
+        NgxAuthFirebaseUIModule.forRoot(environment.firebase, () => 'Genshin Companion', firebaseUiConfig),
         AngularFireModule.initializeApp(environment.firebase),
         AngularFireAuthModule,
         AngularFirestoreModule,
