3953d94c82bd010214fd1e035c0abca48dba46d3
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2020-12-19 21:19:31 +0100

    chore: drop the current migrations as beta ends

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -14,9 +14,6 @@ export interface User {
     providedIn: 'root'
 })
 export class AuthService {
-    private readonly SESSION_USER_KEY = 'session-user';
-    private readonly EMPTY_USER = JSON.stringify(null);
-
     public currentUser = new BehaviorSubject<User>(null);
 
     constructor(
@@ -34,55 +31,6 @@ export class AuthService {
                 this.emitCurrentUser(null);
             }
         });
-    }
-
-    public isUserValid(): boolean {
-        return this.currentUser.value != null;
-    }
-
-    public signUp(username: string, password: string): Promise<firebase.auth.UserCredential> {
-        return this.afAuth.createUserWithEmailAndPassword(username, password);
-    }
-
-    public signIn(username: string, password: string): Promise<firebase.auth.UserCredential> {
-        return this.afAuth.signInWithEmailAndPassword(username, password);
-    }
-
-    public signOut(): Promise<void> {
-        return this.afAuth.signOut();
-    }
-
-    public signInAnonymously(): Promise<firebase.auth.UserCredential> {
-        return this.afAuth.signInAnonymously();
-    }
-
-    public createAccountFromAnonymous(email: string, password: string): Promise<firebase.auth.UserCredential> {
-        return new Promise((resolve, reject) => {
-            const credentials: firebase.auth.AuthCredential = firebase.auth.EmailAuthProvider.credential(email, password);
-
-            const sub = this.afAuth.authState.subscribe((currentUser: firebase.User) => {
-                currentUser.linkWithCredential(credentials)
-                .then(resolve)
-                .catch(reject);
-            });
-
-            sub.unsubscribe();
-        });
-    }
-
-    public getLocalUser(): User {
-        const currentUser = localStorage.getItem(this.SESSION_USER_KEY);
-
-        if (currentUser != null && currentUser !== this.EMPTY_USER) {
-            return JSON.parse(currentUser) as User;
-        }
-
-        return null;
-    }
-
-    public clearLocalUser(): void {
-        localStorage.removeItem(this.SESSION_USER_KEY);
-        this.emitCurrentUser(null);
     }
 
     private emitCurrentUser(user: User): void {
--- src/app/services/migration.service.ts
+++ src/app/services/migration.service.ts
@@ -13,7 +13,7 @@ const WISHES_STORAGE_KEY = 'wishes';
 })
 export class MigrationService {
     private readonly STORAGE_KEY_MIGRATION_QUEUE = 'migration-queue';
-    private readonly VERSIONS_PRE_AUTH = ['1.0.0-rc.1', '1.0.0-rc.2'];
+    private readonly VERSIONS_PRE_AUTH = ['1.0.0-rc.1', '1.0.0-rc.2', '1.0.0-rc.3'];
 
     constructor(
         private readonly firestore: AngularFirestore,
@@ -32,63 +32,6 @@ export class MigrationService {
     public migrate(): Promise<void> {
         const { from, to } = JSON.parse(localStorage.getItem(this.STORAGE_KEY_MIGRATION_QUEUE));
 
-        if (localStorage.getItem(WISHES_STORAGE_KEY)) {
-            return this.migrateFromBeta();
-        } else if (this.VERSIONS_PRE_AUTH.includes(from)) {
-            this.migrateNotAuthVersions();
-        }
-
         return Promise.resolve();
-    }
-
-    private migrateFromBeta(): Promise<void> {
-        const oldBanners: Banner[] = JSON.parse(localStorage.getItem(WISHES_STORAGE_KEY));
-
-        return new Promise((resolve, reject) => {
-            if (oldBanners) {
-                Promise.all(oldBanners.map((banner: Banner) => this.bannerService.saveUserBanner(banner)))
-                .then((_result) => {
-                    localStorage.removeItem(WISHES_STORAGE_KEY);
-                    resolve();
-                })
-                .catch(reject);
-            } else {
-                resolve();
-            }
-        })
-    }
-
-    private migrateNotAuthVersions(): Promise<void> {
-        return new Promise(async (resolve, reject) => {
-            try {
-                // Recover the local user
-                const localUser: User = this.authService.getLocalUser();
-
-                // Recover the banners saved for the local user
-                const banners = await this.bannerService.searchUserBanners(localUser).pipe(first()).toPromise();
-
-                // Clear the custom local user
-                this.authService.clearLocalUser();
-
-                // Create a new anonymous user
-                await this.authService.signInAnonymously();
-
-                // Save the banners under this new user
-                this.authService.currentUser.subscribe(async (user: User) => {
-                    const t = setTimeout(() => {
-                        reject("The migration did not finish successfully. Reload to try again");
-                    }, 60 * 1000);
-
-                    if (user != null) {
-                        await Promise.all(banners.map((banner: Banner) => this.bannerService.saveUserBanner(banner)));
-
-                        clearTimeout(t);
-                        localStorage.removeItem(this.STORAGE_KEY_MIGRATION_QUEUE);
-                    }
-                });
-            } catch (error) {
-                reject(error);
-            }
-        });
     }
 }
